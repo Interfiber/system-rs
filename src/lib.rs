8e7437c54880f5affc5939577e26be737e13a446
Interfiber <stasonis@gmail.com>
2020-09-14 09:29:29 -0400

    Adds a which_exec function

--- src/lib.rs
+++ src/lib.rs
@@ -1,7 +1,33 @@
 pub fn exec(program: &str, args: &str) -> String {
+	//! ```exec``` runs a command and returns its output
+	//! 
+	//!
+	//! Example
+	//! ```
+	//! let cmd = system_rs::exec("ls", "/");
+	//! println!("The output of ls / is: {}", cmd);
+	//! ```
     let split_args = args.split(" ");
     let vec = split_args.collect::<Vec<&str>>();
     let cmd = std::process::Command::new(program).args(&vec).stdout(std::process::Stdio::piped()).output().unwrap();
     let out = String::from_utf8(cmd.stdout).unwrap();
     return out.to_string();
+}
+pub fn which_exists(program: &str) -> bool {
+	//! ```which_exists``` checks if a program exists
+	//! in the path
+	//!
+	//! 
+	//! Example
+	//! ```
+	//! if system_rs::which_exists("rustc") {
+	//!		println!("Rust is not installed!");
+	//! }
+	//! ```
+	let which = exec("/usr/bin/which", program);
+	if which == "" {
+		return false
+	}else {
+		return true
+	}
 }
